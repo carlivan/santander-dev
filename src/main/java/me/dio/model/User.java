173d68147bee5e5bde3914ba19d106d7f5ec3f7e
carlivan <ccarlivan@yahoo.com.br>
2025-08-05 15:00:26 -0300

    inserido variaveis de ambiente para produção no railway

--- src/main/java/me/dio/model/User.java
+++ src/main/java/me/dio/model/User.java
@@ -25,6 +25,9 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
 
+    public User() {
+    }
+
     public Long getId() {
         return id;
     }
